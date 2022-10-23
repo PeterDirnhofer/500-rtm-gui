813429d27c608fff0456de79ac1298d96fcd8e3b
Peter Dirnhofer <peter.dirnhofer@t-online.de>
2022-10-23 14:37:20 +0200

    Adjust frame added

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 # Python Tutorial: GUI Calculator with Model View Controller #1
 # https://youtu.be/ek47NMFW_mk
+import sys
 from tkinter.messagebox import showinfo
 
 import serial
@@ -8,7 +9,7 @@ from model import Model
 from usb_serial import Usb_serial
 from view import View
 import os
-import sys
+from sys import exit
 import time
 from threading import Timer
 
@@ -35,22 +36,28 @@ class Controller:
         )
 
     def select_restart(this):
-
+        this.view.frame_select_com_off()
+        this.view.frame_adjust_off()
+        # send restart to ESP32
         this.usb_serial.write_comport(chr(3))
         this.view.text_com_delete()
-        os.execv(sys.argv[0],sys.argv)
+        this.view.text_com_read_update("RESTART")
+        this.m_old_comport_status = ""
+        this.comport_status="READY"
+
+        os.execv(__file__, sys.argv)
 
     def select_adjust(this):
-        if this.comport_status != "READY":
+        if (this.comport_status != "READY") & (this.comport_status!= "WAIT_FOR_IDLE"):
             showinfo(
                 title='Information',
-                message="STM not connected\nCOM port Status 'READY' needed"
+                message=f"STM not connected\nCOM port Status 'READY' needed\n Status is {this.comport_status}"
             )
             return
 
         this.usb_serial.write_comport('ADJUST')
-        this.view.text_com_delete()
-
+        this.view.frame_adjust_on()
+        this.comport_status="ADJUST"
 
     def handle_com_port(this):
 
@@ -61,20 +68,28 @@ class Controller:
         akt_port = this.usb_serial.get_comport()
         this.view.text_com_port.set(f'{akt_port}: {this.comport_status}' )
 
+
         if this.comport_status == "INIT":
             result = this.usb_serial.open_comport(akt_port)
             this.view.text_status.set(result)
+            # Cannot find COM PORT
+
             if result!='OPEN':
+                this.view.text_com_port.set(f'ERROR Cannot open {akt_port}')
+                this.view.frame_select_com_on()
                 # Display available COM ports
+                #this.view.frame_select_com.grid(row=0, column=1, sticky='nesw')
+
+                #this.view.listbox_comports.pack(padx=10, pady=10)
                 available_ports=this.usb_serial.get_ports()
                 this.view.display_comports(available_ports)
                 if this.view.com_selected!="":
                     this.usb_serial.put_comport(this.view.com_selected)
-                #this.comport_status="INIT"
-            elif result=="ERROR":
-                this.view.text_com_port.set(f'ERROR Cannot open {akt_port}')
+                    this.view.listbox_comports.pack_forget()
 
+                #this.comport_status="INIT"
             else:
+
                 this.comport_status="WAIT_FOR_IDLE"
                 this.usb_serial.start_comport_read_thread()
 
@@ -82,10 +97,12 @@ class Controller:
             if this.usb_serial.read_line=='IDLE':
                 this.comport_status="READY"
 
+        elif this.comport_status=="ADJUST":
+            this.view.text_adjust["text"]="lghlug"
+
         this.view.trigger_comloop(200)
 
 if __name__ == '__main__':
-
 
     test=""
     rtm = Controller()
--- view.py
+++ view.py
@@ -1,21 +1,18 @@
 import ctypes
 import tkinter as tk
-from tkinter import ttk,Text
+from tkinter import ttk
 from tkinter.constants import *
-from PIL import ImageTk, Image
-
 
 dumyMsg = "kI,10\nkP,1000\ndestinationTunnelCurrent,10,0\nremainingTunnelCurrentDifferencenA,0.01\nstarX,0\nstartY," \
           "0\ndirection,0\n "
 dumyMsg += "maxX,0\nmaxY,0\nmultiplicator,10"
 
 
-
 class View(tk.Tk):
     def __init__(self, controller):
         super().__init__()  # Initializes methods of Tk. Tk can be used in View
         self.controller = controller  # controller can be used as attribute in class View
-        self.com_selected=""
+        self.com_selected = ""
         # https://stackoverflow.com/questions/44548176/how-to-fix-the-low-quality-of-tkinter-render
         ctypes.windll.shcore.SetProcessDpiAwareness(True)
 
@@ -54,7 +51,7 @@ class View(tk.Tk):
         button_select_measure.grid(row=0, column=1, padx=10, pady=2)
 
         button_select_adjust = ttk.Button(frame_top, text="Adjust", command=controller.select_adjust)
-        button_select_adjust.grid(row=0, column=2, padx=10,pady=2 )
+        button_select_adjust.grid(row=0, column=2, padx=10, pady=2)
 
         ###################################################################################
         # frame_left  Add frame_com COM READ   COM WRITE  COM SELECT
@@ -66,22 +63,21 @@ class View(tk.Tk):
         frame_com.rowconfigure(2, weight=2)
 
         # COM read ###############################################
-        frame_com_read=ttk.LabelFrame(frame_com,text='COM read')
+        frame_com_read = ttk.LabelFrame(frame_com, text='COM read')
         frame_com_read.grid(row=0, column=0, padx=10, pady=10, sticky=E + W)
         # add a scrollbar https://youtu.be/BckVJoE94Lk
 
-        self.scrollbar=ttk.Scrollbar(frame_com_read,orient='vertical')
-        self.scrollbar.pack(side=RIGHT,fill=Y)
+        self.scrollbar = ttk.Scrollbar(frame_com_read, orient='vertical')
+        self.scrollbar.pack(side=RIGHT, fill=Y)
 
-        self.text_com_read = tk.Text(frame_com_read,width=30,height=10,
-                                     yscrollcommand=self.scrollbar.set,relief='sunken')
-        self.text_com_read.pack(side=TOP,fill=X)
+        self.text_com_read = tk.Text(frame_com_read, width=30, height=10,
+                                     yscrollcommand=self.scrollbar.set, relief='sunken')
+        self.text_com_read.pack(side=TOP, fill=X)
 
         self.scrollbar.config(command=self.text_com_read.yview)
 
-
         # COM write
-        frame_com_write=ttk.LabelFrame(frame_com,text='COM write')
+        frame_com_write = ttk.LabelFrame(frame_com, text='COM write')
         frame_com_write.grid(row=1, column=0, padx=10, pady=5)
 
         label_com_write = ttk.Label(frame_com_write,
@@ -91,9 +87,8 @@ class View(tk.Tk):
 
         # COM port
 
-
-        frame_com_port=ttk.LabelFrame(frame_com,text='COM Port')
-        frame_com_port.grid(row=2,column=0,padx=10)
+        frame_com_port = ttk.LabelFrame(frame_com, text='COM Port')
+        frame_com_port.grid(row=2, column=0, padx=10)
 
         self.text_com_port = tk.IntVar()
         label_com_port = ttk.Label(frame_com_port,
@@ -119,45 +114,75 @@ class View(tk.Tk):
         # frame_bottom add frame_status
 
         frame_status = ttk.LabelFrame(frame_bottom, text="Status")
-        frame_status.grid(row=0,column=0,padx=10, pady=10,sticky='nesw')
+        frame_status.grid(row=0, column=0, padx=10, pady=10, sticky='nesw')
         self.text_status = tk.StringVar()
-        self.label_status=ttk.Label(frame_status,
-                                    textvariable=self.text_status)
-        #self.label_status.after(2000,self.controller.handle_com_port)
-
-        self.label_status.grid(row=0, column=0,padx=10,pady=10,sticky = 'nswe')
-
+        self.label_status = ttk.Label(frame_status,
+                                      textvariable=self.text_status)
+        self.label_status.grid(row=0, column=0, padx=10, pady=10, sticky='nswe')
 
         ####################################################################################
-        # frame_middle add listbox
-        frame_select_com = ttk.LabelFrame(frame_middle,text="Select COM")
-        frame_select_com.grid(row=0,column=1,sticky='nesw')
+        # frame_middle Option1 : add frame_select_com add listbox_comports
 
-        self.listbox = tk.Listbox(frame_select_com,
-                             width=70)
-        self.listbox.grid(row=0,column=0,padx=10,pady=10)
-        self.listbox.bind('<<ListboxSelect>>', self.listboxSelect)
+        self.frame_select_com = ttk.LabelFrame(frame_middle, text="Select COM")
+        self.listbox_comports = tk.Listbox(self.frame_select_com,
+                                           width=70)
+        self.listbox_comports.bind('<<ListboxSelect>>', self.listbox_select)
 
+        ####################################################################################
+        # frame_middle Option 2: add Adjust
+
+        self.frame_adjust = ttk.LabelFrame(frame_middle, text="Adjust")
+        #self.frame_adjust.pack(padx=10, pady=10)
+
+        self.text_adjust = ttk.Label(self.frame_adjust,text="VARTEXT einfügen")
+        #self.text_adjust.pack(padx=10, pady=10)
+
+
+        #self.text_adjust.pack_forget()
+        #self.frame_adjust.pack_forget()
+
+        ################################################################
         # configure style
         self.style = ttk.Style(self)
         self.style.configure('TLabel', relief='sunken')
         self.style.configure('TButton', relief='sunken')
 
-    def display_comports(self,ports):
+    def frame_select_com_on(self):
+        #self.frame_adjust_off()
+        self.frame_select_com.grid(row=0, column=1, sticky='nesw')
+        self.listbox_comports.pack(padx=10, pady=10)
+
+    def frame_select_com_off(self):
+        self.listbox_comports.pack_forget()
+        self.frame_select_com.grid_forget()
+
+
+    def frame_adjust_on(self):
+        #self.frame_select_com_off()
+        self.frame_adjust.pack(padx=10, pady=10)
+        self.text_adjust.pack(padx=10, pady=10)
+
+    def frame_adjust_off(self):
+        self.text_adjust.pack_forget()
+        self.frame_adjust.pack_forget()
+
+
+
+    def display_comports(self, ports):
         '''
         Gets list of actual available COM ports from laptop\
         Displays COM List in listbox_comports
         '''
-        #ports = self.sf.get_ports()
-        self.listbox.delete(0, 'end')
-        portsLen = len(ports)
-        self.listbox.config(height=portsLen)
+        # ports = self.sf.get_ports()
+        self.listbox_comports.delete(0, 'end')
+        ports_len = len(ports)
+        self.listbox_comports.config(height=ports_len)
         for port in ports:
-            self.listbox.insert(END, str(port))
+            self.listbox_comports.insert(END, str(port))
 
     def text_com_read_update(self, string_to_add):
-        self.text_com_read['state']=NORMAL
-        self.text_com_read.insert(END,string_to_add)
+        self.text_com_read['state'] = NORMAL
+        self.text_com_read.insert(END, string_to_add)
         self.text_com_read.insert(END, '\n')
         self.text_com_read['state'] = DISABLED
 
@@ -166,17 +191,16 @@ class View(tk.Tk):
         self.text_com_read.delete(1.0, END)
         self.text_com_read['state'] = DISABLED
 
-
-    def listboxSelect(self,event):
-        line = self.listbox.get(ANCHOR)
+    def listbox_select(self, event):
+        line = self.listbox_comports.get(ANCHOR)
         temp = line.split(" ")[0]
-        self.com_selected=temp
+        self.com_selected = temp
         print(temp)
 
-    def trigger_comloop(self,intervall_ms):
-        self.after(intervall_ms,self.controller.handle_com_port)
-    def main(self):
+    def trigger_comloop(self, intervall_ms):
+        self.after(intervall_ms, self.controller.handle_com_port)
 
-        #self.after(5000,self.controller.handle_com_port)
+    def main(self):
+        # self.after(5000,self.controller.handle_com_port)
         self.trigger_comloop(1000)
         self.mainloop()  # Tk mainloop
