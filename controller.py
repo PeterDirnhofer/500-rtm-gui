73b3d5cf79329f2b4d315b423840d14b151ac15b
Peter Dirnhofer <peter.dirnhofer@t-online.de>
2022-10-25 18:57:07 +0200

    New statemachine. Next ERROR handling

--- controller.py
+++ controller.py
@@ -10,6 +10,7 @@ from usb_serial import Usb_serial
 from view import View
 
 
+
 # Timer Class https://youtu.be/5NJ9cc0dnCM
 class Controller:
     def __init__(self):
@@ -21,6 +22,7 @@ class Controller:
         self.comport_state=""
         self.is_connected=False
         self.act_port=""
+        self.sm_state= 'INIT'
 
     def main(self):
         self.view.main()
@@ -35,6 +37,75 @@ class Controller:
             self.usb_serial.put_comport(self.view.com_selected)
             self.view.text_com_read_update(f'{self.view.com_selected} selected')
         return
+
+    def sm_is_default_port_existing(self):
+        self.act_port = self.usb_serial.get_comport_saved()
+        available_ports = self.usb_serial.get_ports()
+        port_exists = False
+        for port in available_ports:
+            r = self.act_port in port
+            if r == True:
+                port_exists = True
+        if port_exists == False:
+            self.sm_state='ERROR'
+            return
+        else:
+            self.sm_state='EXISTING'
+            return
+
+
+    def sm_open(self):
+        # try to open COM port on computer
+        result = self.usb_serial.open_comport(self.act_port)
+        if result == 'OPEN':
+            self.sm_state='OPEN'
+        else:
+            self.sm_state='ERROR'
+        return
+
+    def sm_send_reset(self):
+        if self.usb_serial.write_comport(chr(3))==True:
+            self.usb_serial.start_comport_read_thread()  # enable receiver
+            self.sm_state = 'WAIT_FOR_IDLE'
+        else:
+            self.sm_state = 'ERROR'
+
+    def sm_wait_for_idle(self):
+        if self.usb_serial.read_line=='IDLE':
+            self.sm_state='COM_READY'
+        else:
+            self.sm_state = 'ERROR'
+
+
+
+    def state_machine(self):
+
+        if self.sm_state== 'INIT':
+            self.sm_is_default_port_existing()
+            self.view.trigger_state_machine(50)
+            return
+        elif self.sm_state== 'EXISTING':
+            self.sm_open()
+            self.view.trigger_state_machine(50)
+            return
+        elif self.sm_state== 'OPEN':
+            self.sm_send_reset()
+            self.view.trigger_state_machine(1000)
+            return
+        elif self.sm_state== 'WAIT_FOR_IDLE':
+            self.sm_wait_for_idle()
+            self.view.trigger_state_machine(50)
+            return
+        elif self.sm_state== 'COM_READY':
+            # state machin no longer needed. No retrigger
+            pass
+        elif self.sm_state== "ERROR":
+            self.view.text_status.set("ERROR !!!!!!!!!!!!!!!!!")
+            pass
+        else:
+            raise Exception(f'Invalid state: {self.sm_state}')
+
+        #self.view.trigger_state_machine(50)
 
     def connect_com(self):
 
@@ -81,7 +152,7 @@ class Controller:
 
         return 'CONNECTED'
 
-    def state_machine(self):
+    def state_machine1(self):
 
         print(f"Status: {self.comport_status}")
         if self.is_connected==False:
